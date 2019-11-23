ed6f3d434ab6a034540cd9a363d6bbf694b24967
ksripathi <sripathik@spaceinsolutions.com>
2019-11-23 20:18:00 +0530

    refactored install/destroy helm scripts

--- helm-chart/destroy-helm.sh
+++ helm-chart/destroy-helm.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+set -euo pipefail
 charts="example-service nginx-ingress prometheus concourse sonarqube"
 for chart in $charts
 do
-    is_chart_exist=`helm ls | grep $chart`
-    if [ ! -z "$is_chart_exist" ]
-    then
-	echo "Deleting $chart..."
-	helm delete --purge $chart
-    fi    
+  is_chart_exist=`helm ls | grep $chart`
+  if [ ! -z "$is_chart_exist" ]
+  then
+      echo "Deleting $chart..."
+      helm delete --purge $chart
+  fi    
 done
--- helm-chart/install-helm.sh
+++ helm-chart/install-helm.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
-example_service=`helm ls | grep example-service`
-if [ ! -z "$example_service" ]
+
+set -euo pipefail
+
+is_example_chart_exist=`helm ls | grep example-service`
+if [ ! -z "$is_example_chart_exist" ]
 then
-      helm upgrade example-service .
+    helm upgrade example-service .
 else
-      helm install . --name example-service
+    helm install . --name example-service
 fi
 
 community_charts="nginx concourse prometheus sonarqube"
 for chart in $community_charts
 do
-    is_chart_exist=`helm ls | grep $chart`
-    if [ ! -z "$is_chart_exist" ]
-    then
-	helm upgrade $chart stable/$chart
-    else
-	helm install stable/$chart --name $chart
-    fi    
+  is_chart_exist=`helm ls | grep $chart`
+  if [ ! -z "$is_chart_exist" ]
+  then
+      helm upgrade $chart stable/$chart
+  else
+      helm install stable/$chart --name $chart
+  fi    
 done
 
 
