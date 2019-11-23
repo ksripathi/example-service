fd0486241b2937eb22b17cf608abb8575bceade7
ksripathi <sripathik@spaceinsolutions.com>
2019-11-23 17:20:50 +0530

    added n1-standard-4

--- infra/main.tf
+++ infra/main.tf
@@ -1,40 +1,26 @@
-resource "google_container_cluster" "gcp_kubernetes" {
-    name               = "${var.cluster_name}"
+resource "google_container_cluster" "primary" {
+  name     = "${var.cluster_name}"
+  location = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  node_locations = [
+    "us-central1-a",
+  ]
+}
 
-    location               = "us-central1-a"
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = "${google_container_cluster.primary.name}"
+  node_count = 2
 
-    initial_node_count = "${var.gcp_cluster_count}"
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-4"
 
-    master_auth {
-
-        username = "${var.linux_admin_username}"
-
-        password = "${var.linux_admin_password}}"
-
-    }
-
-    node_config {
-
-        oauth_scopes = [
-
-          "https://www.googleapis.com/auth/compute",
-
-          "https://www.googleapis.com/auth/devstorage.read_only",
-
-          "https://www.googleapis.com/auth/logging.write",
-
-          "https://www.googleapis.com/auth/monitoring",
-
-        ]
-
-        labels {
-
-            this-is-for = "dev-cluster"
-
-        }
-
-        tags = ["dev", "work"]
-
-    }
-
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 }
