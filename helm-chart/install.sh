0a5402bad3b1ac008961b2c8d9e7fdc2b36468ad
ksripathi <sripathik@spaceinsolutions.com>
2019-11-28 02:15:32 +0530

    refactored helm install steps

---
+++ helm-chart/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+kubectl -n kube-system create serviceaccount tiller || true
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller || true
+helm init --service-account tiller || true
+
+echo "Installing exmaple-service chart"
+helm upgrade example-service . --install
+
