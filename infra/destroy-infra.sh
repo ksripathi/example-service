a48e8cff5c28bedf73f66175136b204bb82529b4
ksripathi <sripathik@spaceinsolutions.com>
2019-11-28 13:39:43 +0530

    improved destroy script

--- infra/destroy-infra.sh
+++ infra/destroy-infra.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 terraform_version=v0.11.14
-terraform_exist=```terraform --version | grep $terraform_version```
+terraform_exist=```./terraform --version | grep $terraform_version || terraform --version | grep $terraform_version```
 if [ -z $terrafrom_exist ]
 then
     echo "Installing $terraform_version"
-    wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+    if [ ! -f terraform_0.11.14_linux_amd64.zip ];then
+       wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+    fi
     unzip terraform_0.11.14_linux_amd64.zip
-    chmod +x terraform
-    ./terraform --version
+    chmod +x ./terraform
+    alias terraform='./terraform'
 else
-    echo "terraform v0.11.14 already exist"
+    echo "terraform $terraform_version already exist"
 fi
-
-./terraform destroy --auto-approve
+terraform --version
+terraform destroy --auto-approve
 
 charts="nginx-ingress prometheus concourse sonarqube"
 for chart in $charts
