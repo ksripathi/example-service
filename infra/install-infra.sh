732c26358acbfef89e7411fd4e938a51190a71f1
ksripathi <sripathik@spaceinsolutions.com>
2019-11-28 13:34:05 +0530

    improved install script

--- infra/install-infra.sh
+++ infra/install-infra.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 terraform_version=v0.11.14
-terraform_exist=```terraform --version | grep $terraform_version```
+terraform_exist=```./terraform --version | grep $terraform_version || terraform --version | grep $terraform_version```
 if [ -z $terrafrom_exist ]
 then
     echo "Installing $terraform_version"
-    wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+    if [ ! -f terraform_0.11.14_linux_amd64.zip ];then
+       wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+    fi
     unzip terraform_0.11.14_linux_amd64.zip
-    chmod +x terraform
-    ./terraform --version
+    chmod +x ./terraform
+    alias terraform=./terraform
 else
-    echo "terraform v0.11.14 already exist"
+    echo "terraform $terraform_version already exist"
 fi
-
-./terraform apply --auto-approve
+terraform --version
+exit 0
+terraform apply --auto-approve
 
 # TODO: this has to be variablized
 gcloud beta container clusters get-credentials devcluster --region us-central1 --project dev-project-259009
